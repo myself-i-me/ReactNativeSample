2f8d6dcb917425884199fa99e7e14d825996ef5a
Ganesh Darode <ganeshdarode121@gmail.com>
2023-06-30 12:53:55 +0530

    project created

--- App.js
+++ App.js
@@ -4,17 +4,24 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.textStyle}>React Native</Text>
+      <Text style={styles.textStyle}>Demo Application</Text>
       <StatusBar style="auto" />
     </View>
   );
 }
 
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#f1d9fa',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  textStyle: {
+    fontSize: 20,
+    fontWeight: 400,
+    fontStyle: 'italic'
+  }
 });
